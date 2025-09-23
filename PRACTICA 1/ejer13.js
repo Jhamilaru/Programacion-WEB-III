5db31c7bec95b5dd0bf8b8ee7431d4e3539da7e5
Jhamilaru <151691520+Jhamilaru@users.noreply.github.com>
2025-09-23 19:59:04 -0400

    ejer13.js

--- PRACTICA 1/ejer13.js
+++ PRACTICA 1/ejer13.js
@@ -1,3 +1,16 @@
+//PROMESAS
+function doble(num) {
+    return new Promise(resolve => {
+        resolve(num * 2);
+    });
+}
+
+doble(2)
+    .then(r => doble(r))
+    .then(r => doble(r))
+    .then(r => console.log(r)); // 16
+
+// CON ASYNC AWAIT
 async function calcular() {
     let r = await doble(2);
     r = await doble(r);
@@ -6,3 +19,4 @@ async function calcular() {
 }
 
 calcular();
+
