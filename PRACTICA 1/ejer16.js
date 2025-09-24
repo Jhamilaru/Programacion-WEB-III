29e24011af07f06e71f45876fa7f6bbf7c3f9029
Jhamilaru <151691520+Jhamilaru@users.noreply.github.com>
2025-09-23 20:00:58 -0400

    ejer16.js

--- PRACTICA 1/ejer16.js
+++ PRACTICA 1/ejer16.js
@@ -1,6 +1,14 @@
+function obtenerDato() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve("Dato obtenido"), 1000);
+    });
+}
+
+obtenerDato().then(d => console.log(d));
 async function obtenerDatoAsync() {
     let d = await obtenerDato();
     console.log(d);
 }
 
 obtenerDatoAsync();
+
